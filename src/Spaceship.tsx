59ebd342e81af3ca1529da6126a15628add02640
yoseyusMainComputer <zaquijobs@gmail.com>
2026-02-05 16:36:17 +0100

    updagrade model

--- src/Spaceship.tsx
+++ src/Spaceship.tsx
@@ -4,7 +4,8 @@ import  { useFrame, type ThreeElements } from "@react-three/fiber";
 type SpaceshipProps = ThreeElements["mesh"] & {velocity: number}
 
 export default function Spaceship({velocity = 0.2,...props}: SpaceshipProps) {
-    const spaceship = useGLTF('/models/spaceship.glb');
+    const spaceship = useGLTF('/models/spaceship_V2.glb');
+    console.log(spaceship.nodes);
     useFrame((_, delta) => {   
         spaceship.nodes.rotorBack.rotation.x -= velocity * delta;
         spaceship.nodes.rotorFront.rotation.x += velocity *  delta;
